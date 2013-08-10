c362c884a6fdc6ae7c7bd914d97de85fe4683b92
jackhughesweb <jack@jackhughesweb.com>
2013-08-10 20:48:18 +0100

    New rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hydroelectric::Application.config.secret_token = '5512f69dda30ef3092688c5ba4a49fa8419718f244fdd67a2fd0c90359576b430624254d36b9a56443f371c60b8f5b30078c07e7f83dfd2d8e7d38e3ef9bc189'
