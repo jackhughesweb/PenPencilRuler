54e3d5baf3bb57ac32d533188ac407e9538540f5
jackhughesweb <jack@jackhughesweb.com>
2013-08-25 11:21:42 +0100

    Added pg gem

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :deploy_via, :remote_cache
 set :use_sudo, false
 
 set :scm, "git"
-set :repository, "git@github.com:jackhughesweb/#{application}.git"
+set :repository, "git@github.com:jackhughesweb/PenPencilRuler.git"
 set :branch, "master"
 
 default_run_options[:pty] = true
