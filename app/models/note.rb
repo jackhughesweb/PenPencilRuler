46e93d96970949d06fc96af8332adcb6c034c0ad
jackhughesweb <jack@jackhughesweb.com>
2013-09-27 19:39:47 +0100

    New alert style and change spacing between note title and note text

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,5 +2,6 @@ class Note < ActiveRecord::Base
   attr_accessible :text, :topic_id, :title
   belongs_to :topic
   belongs_to :user
+  validates :title, presence: true
   validates :text, presence: true
 end
